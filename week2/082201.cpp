a6afa21bdbc3d00ab1b4932efcc4d8c0de43737e
Eddie Yen <b04303117@ntu.edu.tw>
2020-08-23 00:30:42 +0800

    0822

---
+++ week2/082201.cpp
@@ -0,0 +1,23 @@
+//Statement 1 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//單一敘述如下
+	;
+	//複合敘述如下
+	{
+
+	}
+	//declaration
+	int a = 10;
+	//assingment
+	a = 1;//此也可以當成“值” 被輸出
+	cout << (a = 4) << endl;
+	cout << --a << endl;
+	cout << a++ << endl;
+
+	return 0;
+
+}
