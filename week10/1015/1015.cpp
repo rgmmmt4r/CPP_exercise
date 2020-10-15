693e5931aac5132e22b134cda795560003e8bf03
Eddie Yen <b04303117@ntu.edu.tw>
2020-10-16 00:50:46 +0800

    1015

---
+++ week10/1015/1015.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y;
+	for(x=1;x<=5;x++){
+		for(y=1;y<=x;y++){
+			cout <<y << " ";
+		}
+		cout << endl;
+	}	
+	cout << "========="<< endl ;
+	for(x=5;x>=1;x--){
+		for(y=1;y<=x;y++){
+			cout <<y << " ";
+		}
+		cout << endl;
+	}	
+	cout << "========="<< endl ;
+	for(x=1;x<=5;x++){
+		for(y=5;y>=x;y--){
+			cout <<y << " ";
+		}
+		cout << endl;
+	}	
+	cout << "========="<< endl ;
+	for(x=1;x<=5;x++){
+		for(y=4;y>=x;y--){
+			cout << " ";
+		}
+		for(y=1;y<=2*x-1;y++){
+			cout << y;
+		}
+		cout <<  endl;
+	}
+	for(x=4;x>=1;x--){
+		for(y=4;y>=x;y--){
+			cout << " ";
+		}
+		for(y=1;y<=2*x-1;y++){
+			cout << y;
+		}
+		cout <<  endl;
+	}
+
+	return 0;
+}
