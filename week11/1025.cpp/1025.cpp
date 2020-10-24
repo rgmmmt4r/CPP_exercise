8f95b78d00a73c31135596174e00d1935b1e61a9
Eddie Yen <b04303117@ntu.edu.tw>
2020-10-25 00:08:26 +0800

    1025

---
+++ week11/1025.cpp/1025.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int ary1[][3] ={{1,2,3},{4,5,6}};
+	int ary2[2][3] = {2};
+	for(int i=0;i<2;i++){
+		for(int j=0;j<3;j++){
+			cout << ary1[i][j] << " ";
+		}
+		cout << endl;
+	}   
+	for(int i=0;i<2;i++){
+		for(int j=0;j<3;j++){
+			cout << ary2[i][j] << " ";
+		}
+		cout << endl;
+	}  
+	return 0;   
+}
